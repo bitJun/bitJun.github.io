b85a91d5710accc7829310474ba1473c34f45307
gengyi <609924321@qq.com>
2022-05-19 11:42:57 +0800

    1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import './index.css';
 import App from './App';
 import Router from './router';
 import reportWebVitals from './reportWebVitals';
-
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/workboxServiceWorker.js').then(registration => {
+      console.log('SW registered: ', registration)
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError)
+    })
+  })
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
